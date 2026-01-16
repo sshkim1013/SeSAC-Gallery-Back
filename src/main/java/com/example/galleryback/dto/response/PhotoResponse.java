23a4079d0f2c47d2a3d106029326bbee43103f6f
sshkim1013 <sshkim1013@naver.com>
2026-01-16 10:13:19 +0900

    entity, repository, dto

---
+++ src/main/java/com/example/galleryback/dto/response/PhotoResponse.java
@@ -0,0 +1,28 @@
+package com.example.galleryback.dto.response;
+
+import com.example.galleryback.entity.Photo;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class PhotoResponse {
+
+    private Long id;
+
+    private String title;
+
+    private String description;
+
+    private String imageUrl;
+
+    public static PhotoResponse from(Photo photo) {
+        return PhotoResponse.builder()
+                .id(photo.getId())
+                .title(photo.getTitle())
+                .description(photo.getDescription())
+                .imageUrl(photo.getImageUrl())
+                .build();
+    }
+
+}
--- src/main/java/com/example/galleryback/entity/Photo.java
+++ src/main/java/com/example/galleryback/entity/Photo.java
@@ -1,5 +1,39 @@
 package com.example.galleryback.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "photo")
+@Getter
+@NoArgsConstructor
 public class Photo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(length = 100)
+    private String description;
+
+    @Column(nullable = false, length = 500)
+    private String imageUrl;
+
+    @Builder
+    public Photo(String title, String description, String imageUrl) {
+        this.title = title;
+        this.description = description;
+        this.imageUrl = imageUrl;
+    }
 
 }
